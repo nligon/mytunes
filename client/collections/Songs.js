68ea88bf6a76335c73356f6f5c72ea48038996f8
Brian Kilrain <bkilrain@gmail.com>
2016-08-11 20:25:43 -0700

    Implement Server access for libraryView

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,44 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
-
+  
+  url: 'https://api.parse.com/1/classes/songs/',
   model: SongModel,
+  parse: function(response) {
+    return response.results;
+  },
+  initialize: function() {
+    this.fetch();
+  }
+
+  
+  
+
+
+
+
+
+
+
+
+
+  // initialize: function() {
+  //   $.ajax({
+  //     url: 'https://api.parse.com/1/classes/songs/',
+  //     type: 'GET',
+  //     datatype: 'json',
+  //     success: function(data) {
+  //       console.log(this);
+  //     },
+  //     error: function(error) {
+  //       console.log('error: ', error);
+  //     }
+
+
+  //   });
+  // },
+
+  
+
+
 
 });
